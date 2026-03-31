751e66068f81a9e927f562e62424deb1e55328fd
Arnaud Lamoureux <a.jl.lamoureux@gmail.com>
2026-03-31 11:14:32 +0200

    build() : upgrade packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import copy from "rollup-plugin-copy";
-import pkg from "./package.json";
+import pkg from "./package.json" with { type: "json" };
 
 const comments = function(_node, comment) {
   const { value, type } = comment;
@@ -34,7 +34,6 @@ export default [
     ],
     plugins: [
       typescript({
-        typescript: require("typescript"),
         tsconfig: "tsconfig.json"
       }),
       terser({
@@ -46,7 +45,7 @@ export default [
         targets: [
           { src: "dist/index.js", dest: "docs/public", rename: "lib.js" }
         ],
-        hook: "writeBundle"
+        hook: "closeBundle"
       })
     ]
   }
